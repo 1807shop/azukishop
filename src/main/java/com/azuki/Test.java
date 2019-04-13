e5b5331aca763abf8795c6294b0b53ef344d2894
Jason <jasonjiang96@163.com>
2019-04-13 15:55:31 +0800

    Jason提交

---
+++ src/main/java/com/azuki/Test.java
@@ -0,0 +1,13 @@
+package com.azuki;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Jason
+ * Date: 2019/4/1315:55
+ * Description: No Description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
